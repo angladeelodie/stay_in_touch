f1570ca190e907cd7723ae1ab6edba560fa9e0c6
Elodie <elodie.anglade@gmail.com>
2021-05-11 12:47:39 +0200

    added one dictionnary

--- JS/App.js
+++ JS/App.js
@@ -25,6 +25,11 @@ class App {
     this.members = [];
     this.isDiscordReady = false;
     this.initListeners();
+
+    this.chart1;
+    this.chart2;
+    this.chart3;
+    this.chart4;
   }
 
   initListeners() {
@@ -49,7 +54,7 @@ class App {
     });
 
     console.log("list filled");
-    this.drawChart();
+    this.drawAllCharts();
   }
 
   // fillData(){
@@ -59,55 +64,60 @@ class App {
   //     console.log("data filled");
   // }
 
-  drawChart() {
-    //console.log("drawing");
+  drawChart1(){
     var data1 = require("./JS/ChartData/data1.json");
-    var data2 = require("./JS/ChartData/data2.json");
-    var data3 = require("./JS/ChartData/data3.json");
-    var data4 = require("./JS/ChartData/data4.json");
-
     var options1 = require("./JS/ChartData/options1.json");
-    var options2 = require("./JS/ChartData/options2.json");
-    var options3 = require("./JS/ChartData/options3.json");
-    var options4 = require("./JS/ChartData/options4.json");
+    var chart1canvas = document.getElementById("chart1");
 
-    var chart1 = document.getElementById("chart1");
-    var chart2 = document.getElementById("chart2");
-    var chart3 = document.getElementById("chart3");
-    var chart4 = document.getElementById("chart4");
-
-    // Chart.defaults.font.size = 26;
-    Chart.defaults.font.family = "Oswald";
-    if (this.chartsPage1Drawn == false) {
-      if (chart1 != null) {
-        var radarChart = new Chart(chart1, {
-          type: "radar",
-          data: data1,
-          options: options1,
-        });
-      }
-
-      if (chart2 != null) {
-        var barChart = new Chart(chart2, {
-          type: "bar",
-          data: data2,
-          options: options2,
-        });
-      }
-
-      if (chart3 != null) {
-        var bubbleChart = new Chart(chart3, {
-          type: "bubble",
-          data: data3,
-          options: options3,
-        });
-      }
+    if (chart1canvas != null) {
+      this.chart1 = new Chart(chart1canvas, {
+        type: "radar",
+        data: data1,
+        options: options1,
+      });
     }
-    this.chartsPage1Drawn = true;
+  }
 
-    if (chart4 != null) {
+  drawChart2(){
+    var data2 = require("./JS/ChartData/data2.json");
+    var options2 = require("./JS/ChartData/options2.json");
+    var chart2canvas = document.getElementById("chart2");
+
+    if (chart2canvas != null) {
+      this.chart2 = new Chart(chart2canvas, {
+        type: "bar",
+        data: data2,
+        options: options2,
+      });
+    }
+  }
+
+  drawChart3(){
+    var data3 = require("./JS/ChartData/data3.json");
+    var options3 = require("./JS/ChartData/options3.json");
+    var chart3canvas = document.getElementById("chart3").getContext("2d");;
+
+    if (chart3canvas != null) {
+      
+      this.chart3 = new Chart(chart3canvas, {
+        type: "bubble",
+        data: data3,
+        options: options3,
+      });
+    }
+
+    
+  }
+
+  drawChart4(){
+    var data4 = require("./JS/ChartData/data4.json");
+    var options4 = require("./JS/ChartData/options4.json");
+    var chart4canvas = document.getElementById("chart4");
+
+
+    if (chart4canvas != null) {
       console.log("chart 4");
-      var radarChart = new Chart(chart4, {
+      this.chart4 = new Chart(chart4canvas, {
         type: "radar",
         data: data4,
         options: options4,
@@ -115,36 +125,63 @@ class App {
     }
   }
 
+  drawAllCharts() {
+    Chart.defaults.font.family = "Oswald";
+    if (this.chartsPage1Drawn == false) {
+      this.drawChart1();
+      this.drawChart2();
+      this.drawChart3();
+    }
+    this.chartsPage1Drawn = true;
+    this.drawChart4();
+ 
+  }
+
   onMessage(event, message) {
-    // console.log(event);
-    //console.log(message);
-    // this.messageCount++;
-    // let characterIncrement = message.length;
-    // this.characterCount = this.characterCount + characterIncrement;
-    // let averageLetter = 0;
-    // console.log(message);
-    // let wordCountDiv = document.createElement("div");
-    // let wordCountH2 = document.createElement("span");
-    // let wordCountNumber = document.createElement("span");
-    // let newMessage = document.createTextNode(message);
-    // let wordCount = document.createTextNode(message.length);
-    // wordCountH2.appendChild(newMessage);
-    // wordCountNumber.appendChild(wordCount);
-    // wordCountNumber.classList.add("wordCountNumber");
-    // wordCountDiv.classList.add("wordCountDiv");
-    // wordCountDiv.appendChild(wordCountNumber);
-    // wordCountDiv.appendChild(wordCountH2);
-    // document.getElementById("wordCount").appendChild(wordCountDiv);
-    // document.getElementById("averageLetters").textContent =
-    //   this.characterCount / this.messageCount;
-    //console.log(message);
-    // if (message == "L") {
+    
+    const data3json = "./JS/ChartData/data3.json";
+    const data3 = require(data3json);
+
+    const loveJson = "./JS/Dictionnary/love.json";
+    const love = require(loveJson);
+
+    //checker si le message envoyé fait partie du dictionnaire "love"
+    if(love.hasOwnProperty(message)){
+      //on récupère les points indiqués à ce message dans le dicitonnaire "love"
+      var points = love[message];
+      console.log(points);
+    }
+
+    //   for(let i=0; i<data3.datasets.length; i++){
+    //   data3.datasets[i].data[0].r = 15;
     // }
+
+
+    //on ajoute les points récupérés grâce à ce message au rayon du cercle "love"
+    var currPoints = data3.datasets[2].data[0].r
+    data3.datasets[2].data[0].r = currPoints + points
+  
+  
+    fs.writeFile(data3json, JSON.stringify(data3), function writeJSON(err) {
+      if (err) return console.log(err);
+      //console.log(JSON.stringify(data3));
+      //console.log('writing to ' + data3json);
+    });
+
+    this.chart3.destroy();
+    this.drawChart3();
+    
+
+    
+    
+    //console.log(data3);
+
+   
   }
 }
 window.onload = () => {
   app = new App();
-  app.drawChart();
+  app.drawAllCharts();
 };
 
 // $(document).ready(function () {
